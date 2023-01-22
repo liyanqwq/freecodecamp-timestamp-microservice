e9f774f3be6c6f96b2242bef415539c26311323e
立言 LiYan <58519324+liyanqwq@users.noreply.github.com>
2023-01-22 09:51:23 +0000

    First Commit

--- index.js
+++ index.js
@@ -8,25 +8,37 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+// Timestamp API
+
+app.get("/api/:time", function(req, res) {
+  let time = req.params.time;
+  let date;
+  if (isNaN(time)) {
+    date = new Date(time)
+  } else {
+    date = new Date(new Number(time));
+  }
+  res.json({ "unix": date.getTime(), "utc": date.toGMTString() });
 });
 
 
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
